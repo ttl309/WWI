93233140cccee1f6acf533f7c0fb06f154a582c2
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-03-24 16:30:09 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap" rel="stylesheet">
+    <title>WWI</title>
+</head>
+
+<body>
+    <div class="contact-us">
+        <div class="title">
+          <h1>Contactez-Nous !</h1>
+        </div>
+        <div class="form">
+          <div class="form-items">
+            <input type="text" class="input" placeholder="Username">
+            <i class="fas fa-user"></i>
+          </div>
+          <div class="form-items">
+            <input type="text" class="input" placeholder="Email">
+            <i class="fas fa-envelope"></i>
+          </div>
+          <div class="form-items">
+            <textarea class="input message" cols="30" rows="10" placeholder="Message....."></textarea>
+          </div>
+        </div>
+        
+        <div class="btn">
+          Envoyer
+          <i class="fas fa-arrow-right"></i>
+        </div>
+        
+        
+        
+        
+      </div>
+</body>
+
+</html>
